73bbd42431fa5f26b1a61d1e4c77baa7835b1d91
Jake <github@jake.otherinbox.com>
2010-07-31 20:12:17 -0700

    Implemented a controller for realistic testing.  Had to add some model code as well.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to(@post, :notice => 'Post was successfully created.')
+		else
+			render :action => "new"
+		end
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.attributes.merge!(params[:post])
+		if @post.save
+			redirect_to(@post, :notice => 'Post was successfully updated.')
+		else
+			render :action => "edit"
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to(posts_url)
+	end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveResource::Base
 	self.site = "http://localhost:8888" # AwsSdbProxy host + port
 	self.prefix = "/my_new_domain/"     # use your SimpleDB domain enclosed in /s
+
+	def self.all
+		find(:all)
+	end
+
+	def title
+		self.attributes['title'] || 'undefined'
+	end
+
+	def body
+		self.attributes['body'] || 'undefined'
+	end
+
 end
